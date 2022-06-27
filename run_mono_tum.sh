604e8e8ed1812ba8dc30b1cebacdc544b35da416
ryuei_washida <ryuei_washida@nsitexe.co.jp>
2022-06-27 14:38:39 +0900

    ORB-SLAM2 and Thirdparty apps modified to static link

--- run_mono_tum.sh
+++ run_mono_tum.sh
@@ -1,6 +1,3 @@
-cp lib/cygORB_SLAM2.dll .
 cp eigen/opencv/build/bin/cygopencv*.dll .
-cp Thirdparty/DBoW2/lib/cygDBoW2.dll .
-cp Thirdparty/g2o/lib/cygg2o.dll .
 Examples/Monocular/mono_tum Vocabulary/ORBvoc.txt Examples/Monocular/TUM1.yaml dataset/rgbd_dataset_freiburg1_rpy
 rm -f cyg*.dll
